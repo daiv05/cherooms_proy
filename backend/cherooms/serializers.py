bea39f5c7614158ce23c18c69be6946f9c0624e1
David Alejandro Deras Cerros <dc19019@ues.edu.sv>
2022-11-19 10:34:28 -0600

    add to_representation -1

--- backend/cherooms/serializers.py
+++ backend/cherooms/serializers.py
@@ -1,14 +1,36 @@
 from rest_framework import serializers
 from .models import *
 
+class RelatedFieldAlternative(serializers.PrimaryKeyRelatedField):
+    def __init__(self, **kwargs):
+        self.serializer = kwargs.pop('serializer', None)
+        if self.serializer is not None and not issubclass(self.serializer, serializers.Serializer):
+            raise TypeError('"serializer" is not a valid serializer class')
+
+        super().__init__(**kwargs)
+
+    def use_pk_only_optimization(self):
+        return False if self.serializer else True
+
+    def to_representation(self, instance):
+        if self.serializer:
+            return self.serializer(instance, context=self.context).data
+        return super().to_representation(instance)
+
 # ---------------------------------------------------------------------------------
 # ---------------------------------------------------------------------------------
-# ---------------------------------------------------------------------------------
+# --------------ESTE ES EL EJEMPLO, la funcion to_representation IGNORAR
+# ------------------------LUEGO LA ESTARÉ AGREGANDO--------------------------------
 # ---------------------------------------------------------------------------------
 class PerfilUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = PerfilUser
         fields = '__all__'
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['ciudad_id'] = CiudadSerializer(instance.ciudad_id).data
+        return response
 # ---------------------------------------------------------------------------------
 # ---------------------------------------------------------------------------------
 # ---------------------------------------------------------------------------------
@@ -18,15 +40,30 @@ class DepartamentoSerializer(serializers.ModelSerializer):
         model = Departamento
         fields = '__all__'
 
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['pais_id'] = PaisSerializer(instance.pais_id).data
+        return response
+
 class HistorialBusquedaSerializer(serializers.ModelSerializer):
     class Meta:
         model = HistorialBusqueda
         fields = '__all__'
+    
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['perfil_id'] = PerfilUserSerializer(instance.perfil_id).data
+        return response
 
 class PublicacionAlquilerSerializer(serializers.ModelSerializer):
     class Meta:
         model = PublicacionAlquiler
         fields = '__all__'
+    
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['perfil_id'] = PerfilUserSerializer(instance.perfil_id).data
+        return response
 
 class FotoSerializer(serializers.ModelSerializer):
     class Meta:
