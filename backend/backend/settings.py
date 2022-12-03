78ad036789435f8fcbce6bd812a9fbf7e2560781
David Alejandro Deras Cerros <dc19019@ues.edu.sv>
2022-12-02 20:10:27 -0600

    probando staticfiles

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -144,8 +144,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = 'staticfiles/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
